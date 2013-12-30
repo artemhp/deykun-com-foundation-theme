a6814fe2b160c3906a65d36a359fa17d082ad9c4
Artem Deykun <artem@inflection.com>
2013-12-30 02:29:53 +0200

    header, navigation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+
+$(document).foundation({
+    orbit: {
+        animation: 'fade',
+        timer_speed: 30000,
+        pause_on_hover: true,
+        animation_speed: 500,
+        bullets: false,
+        next_on_click: true,
+        navigation_arrows: false,
+        slide_number: false,
+        timer: false
+    },
+    topbar : {
+        custom_back_text: false, // Set this to false and it will pull the top level link name as the back text
+    }
+});
